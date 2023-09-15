1b17c61cf2b09ac9b74339d7450d17a99debfa77
zhaoyunlong <zhaoyunlong@pin-dao.cn>
2023-09-15 14:42:15 +0800

    L26 删除有序数组中的重复项

---
+++ src/面试经典150题/L26_删除有序数组中的重复项.java
@@ -0,0 +1,73 @@
+package 面试经典150题;
+
+/**
+ * 给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
+ * 考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过：
+ * 更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。nums 的其余元素与 nums 的大小不重要。
+ * 返回 k 。
+ * 判题标准:
+ * <p>
+ * 系统会用下面的代码来测试你的题解:
+ * <p>
+ * int[] nums = [...]; // 输入数组
+ * int[] expectedNums = [...]; // 长度正确的期望答案
+ * <p>
+ * int k = removeDuplicates(nums); // 调用
+ * <p>
+ * assert k == expectedNums.length;
+ * for (int i = 0; i < k; i++) {
+ * assert nums[i] == expectedNums[i];
+ * }
+ * 如果所有断言都通过，那么您的题解将被 通过。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * 输入：nums = [1,1,2]
+ * 输出：2, nums = [1,2,_]
+ * 解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素。
+ * <p>
+ * 示例 2：
+ * 输入：nums = [0,0,1,1,1,2,2,3,3,4]
+ * 输出：5, nums = [0,1,2,3,4]
+ * 解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素。
+ */
+public class L26_删除有序数组中的重复项 {
+
+    /**
+     * 双指针
+     */
+    public int removeDuplicates(int[] nums) {
+        // 注意left从1开始
+        int left = 1;
+        int right = 1;
+
+        while (right < nums.length) {
+            if (nums[right] != nums[right - 1]) {
+                nums[left] = nums[right];
+                left++;
+            }
+            right++;
+        }
+        return left;
+    }
+
+    public int removeDuplicates2(int[] nums) {
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                nums[idx++] = nums[i];
+            }
+        }
+        return idx;
+    }
+
+    public static void main(String[] args) {
+        L26_删除有序数组中的重复项 test = new L26_删除有序数组中的重复项();
+
+        int[] nums = new int[]{1, 1, 2};
+        int newLen = test.removeDuplicates2(nums);
+
+        System.out.println(newLen);
+    }
+}
