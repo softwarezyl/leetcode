c88c3ea97784b2fb3ca2527df94cbea869341660
zhaoyunlong <zhaoyunlong@pin-dao.cn>
2023-09-21 16:31:10 +0800

    1 两数之和

---
+++ src/面试经典150题/哈希表/L1_两数之和.java
@@ -0,0 +1,47 @@
+package 面试经典150题.哈希表;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 你可以按任意顺序返回答案。
+ * <p>
+ * 示例 1：
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * <p>
+ * 示例 2：
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * <p>
+ * 示例 3：
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ */
+public class L1_两数之和 {
+
+    /**
+     * 哈希表
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (map.containsKey(target - num)) {
+                return new int[]{i, map.get(target - num)};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    /**
+     * 双指针
+     */
+}
