ca738897a26a9c6e7e6f55f1c00e7d3aca7e6461
zhaoyunlong <zhaoyunlong@pin-dao.cn>
2023-09-25 13:57:42 +0800

    3 无重复字符的最长子串

---
+++ src/面试经典150题/滑动窗口/L3_无重复字符的最长子串.java
@@ -0,0 +1,59 @@
+package 面试经典150题.滑动窗口;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ * <p>
+ * 示例 1:
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * <p>
+ * 示例 2:
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * <p>
+ * 示例 3:
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ */
+public class L3_无重复字符的最长子串 {
+
+    /*
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while(i<n && j<n){
+            if(!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j-i);
+            }else{
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+
+        int result = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // 判断右指针的元素是否存在
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                result = Math.max(result, j - i);
+            }else{
+                set.remove(s.charAt(i++));
+            }
+        }
+        return result;
+    }
+}
