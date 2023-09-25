076f01d70b2b737f6b668f73b0dcdb4a5dbf19ba
zhaoyunlong <zhaoyunlong@pin-dao.cn>
2023-09-25 14:00:36 +0800

    删除无用代码

--- src/面试经典150题/滑动窗口/L3_无重复字符的最长子串.java
+++ src/面试经典150题/滑动窗口/L3_无重复字符的最长子串.java
@@ -24,21 +24,8 @@ import java.util.Set;
  */
 public class L3_无重复字符的最长子串 {
 
-    /*
-    public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        Set<Character> set = new HashSet<>();
-        int ans = 0, i = 0, j = 0;
-        while(i<n && j<n){
-            if(!set.contains(s.charAt(j))){
-                set.add(s.charAt(j++));
-                ans = Math.max(ans, j-i);
-            }else{
-                set.remove(s.charAt(i++));
-            }
-        }
-        return ans;
-    }
+    /**
+     * 滑动窗口
      */
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
