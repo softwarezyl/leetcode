68cb6a1510e438e0b9ca35bf108face3d758e897
zhaoyunlong <zhaoyunlong@pin-dao.cn>
2023-10-17 04:09:20 +0800

    141 环形链表

---
+++ src/面试经典150题/链表/L141_环形链表.java
@@ -0,0 +1,19 @@
+package 面试经典150题.链表;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class L141_环形链表 {
+
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while(head != null){
+            if(set.contains(head)){
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
---
+++ src/面试经典150题/链表/ListNode.java
@@ -0,0 +1,13 @@
+package 面试经典150题.链表;
+
+public class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
