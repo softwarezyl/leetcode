d60e145f3f4781ab01f07c7c0e00e3acd04a32a5
zhaoyunlong <zhaoyunlong@pin-dao.cn>
2023-10-17 04:24:11 +0800

    2 两数相加

---
+++ src/面试经典150题/链表/L2_两数相加.java
@@ -0,0 +1,52 @@
+package 面试经典150题.链表;
+
+/**
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 示例 1：
+ * 输入：l1 = [2,4,3], l2 = [5,6,4]
+ * 输出：[7,0,8]
+ * 解释：342 + 465 = 807.
+ * <p>
+ * 示例 2：
+ * 输入：l1 = [0], l2 = [0]
+ * 输出：[0]
+ * <p>
+ * 示例 3：
+ * 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * 输出：[8,9,9,9,0,0,0,1]
+ */
+public class L2_两数相加 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+
+        // 携带是否进1
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+
+            int sum = val1 + val2 + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+            current.next = new ListNode(sum);
+
+            current = current.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        if(carry == 1){
+            current.next = new ListNode(1);
+        }
+        return head.next;
+    }
+}
