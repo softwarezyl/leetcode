8c8d834b13ea470825714f3a9361171fb76f24d8
zhaoyunlong <zhaoyunlong@pin-dao.cn>
2023-10-19 19:10:54 +0800

    L92_反转链表II

--- src/面试经典150题/链表/L92_反转链表II.java
+++ src/面试经典150题/链表/L92_反转链表II.java
@@ -4,16 +4,89 @@ import common.ListNode;
 
 /**
  * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
- *
+ * <p>
  * 示例 1：
  * 输入：head = [1,2,3,4,5], left = 2, right = 4
  * 输出：[1,4,3,2,5]
- *
+ * <p>
  * 示例 2：
  * 输入：head = [5], left = 1, right = 1
  * 输出：[5]
  */
 public class L92_反转链表II {
 
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+
+        // 获取子串的前一个节点
+        ListNode pre = dummyNode;
+        for(int i=0; i<left-1; i++){
+            pre = pre.next;
+        }
+        System.out.println("pre.val=" + pre.val);
+
+        // 获取到子串右节点
+        ListNode rightNode = pre;
+        for(int i=0; i<right-left+1; i++){
+            rightNode = rightNode.next;
+        }
+
+        // 获取子串的左节点
+        ListNode leftNode = pre.next;
+        ListNode last = rightNode.next;
+
+        // 切断子串
+        pre.next = null;
+        rightNode.next = null;
+
+        // 反转子串
+        this.reverse(leftNode);
+
+        // 拼接结果
+        pre.next = rightNode;
+        leftNode.next = last;
+        return dummyNode.next;
+    }
+
+    private void reverse(ListNode head){
+        ListNode node = head;
+
+        ListNode pre = null;
+        while(node != null){
+            ListNode next = node.next;
+
+            node.next = pre;
+            pre = node;
+
+            node = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        L92_反转链表II test = new L92_反转链表II();
+        ListNode listNode = test.arrToListNode(new int[]{1, 2, 3, 4, 5});
+//        test.printListNode(listNode);
+
+        test.reverseBetween(listNode, 2, 4);
+    }
+
+    public ListNode arrToListNode(int[] arr){
+        ListNode head = new ListNode(arr[0]);
+
+        ListNode cur = head;
+        for(int i=1; i<arr.length; i++){
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+        }
+        return head;
+    }
+
+    private void printListNode(ListNode node){
+        while(node != null){
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
 
 }
