0d5c0b3c27780bf9422e8ce5258d23926facfabd
zhaoyunlong <zhaoyunlong@pin-dao.cn>
2023-10-26 16:17:55 +0800

    L100_相同的树

---
+++ src/面试经典150题/二叉树/L100_相同的树.java
@@ -0,0 +1,36 @@
+package 面试经典150题.二叉树;
+
+import common.TreeNode;
+
+/**
+ * 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ * <p>
+ * 示例 1：
+ * 输入：p = [1,2,3], q = [1,2,3]
+ * 输出：true
+ * <p>
+ * 示例 2：
+ * 输入：p = [1,2], q = [1,null,2]
+ * 输出：false
+ * <p>
+ * 示例 3：
+ * 输入：p = [1,2,1], q = [1,1,2]
+ * 输出：false
+ */
+public class L100_相同的树 {
+
+    // 深度优先
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
